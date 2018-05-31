b7ed3462657868044168adeb302fe07b41a6eb9a
Marc Longoria <marclongoria@outlook.com>
2018-05-31 00:43:16 -0500

    add db context

---
+++ SportsStore/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
